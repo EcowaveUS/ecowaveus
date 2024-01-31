aae289dc4a870fe4201102a99fcc602cd2746179
Mariano Ostric <ostricmariano@gmail.com>
2024-01-31 15:40:07 -0300

    updated

--- src/components/pagesComponents/Inovation.tsx
+++ src/components/pagesComponents/Inovation.tsx
@@ -1,15 +1,6 @@
 import React,{ } from 'react'
 import styled from 'styled-components'
 
-// function srcset(image: string, size: number, rows = 1, cols = 1) {
-//   return {
-//     src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
-//     srcSet: `${image}?w=${size * cols}&h=${
-//       size * rows
-//     }&fit=crop&auto=format&dpr=2 2x`,
-//   };
-// }
-
 interface Props {
   images: {
     img: string,
