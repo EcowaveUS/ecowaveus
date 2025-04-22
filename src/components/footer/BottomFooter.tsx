e1d22124240de912f22df056a41da8dec1daf210
sweetappleplus <sweetappleplus@gmail.com>
2025-04-22 22:33:08 +0300

    Improved the bottom footer

--- src/components/footer/BottomFooter.tsx
+++ src/components/footer/BottomFooter.tsx
@@ -1,62 +1,113 @@
-import { Box, Button, Typography } from "@mui/material";
-import { useNavigate } from "react-router";
+import { Box, Typography, useMediaQuery } from "@mui/material";
+import { Theme } from "@mui/material/styles";
+import { Link } from "react-router-dom";
+import {
+  FaFacebook,
+  FaFacebookF,
+  FaInstagram,
+  FaLinkedinIn,
+} from "react-icons/fa";
 
 export const BottomFooter = () => {
-  const navigate = useNavigate();
+  const isDesktop = useMediaQuery((theme: Theme) => theme.breakpoints.up("sm"));
   return (
-    <Box
-      sx={{
-        display: "flex",
-        width: "100%",
-        flexDirection: {
-          xs: "column",
-          sm: "row",
-        },
-        justifyContent: "space-between",
-        alignItems: "center",
-        margin: "1rem 0",
-      }}
-    >
-      <Typography variant='body2' color="#fff">
-        © 2021 Ecowave. All Rights Reserved.
-      </Typography>
-      <Box>
-        <Button
-          onClick={() => navigate("/privacy-policy")}
-          variant={"text"}
-          sx={{
-            textTransform: "none",
-            fontFamily: "InterMedium",
-            fontSize: "0.8rem",
-            textDecoration: "underline",
-            color: "#fff",
-            "&:hover": {
-              textDecoration: "underline",
-              backgroundColor: "transparent",
-              color: "#ababab",
-            },
-          }}
+    <Box component="div">
+      <Box
+        component="div"
+        height="1px"
+        bgcolor={isDesktop ? "#FFFFFF0D" : "#D8D8DE"}
+        width="100%"
+        marginX={isDesktop ? "0" : "20px"}
+      ></Box>
+      <Box
+        component="div"
+        className="container1"
+        paddingTop={isDesktop ? "32px" : "24px"}
+        paddingBottom={isDesktop ? "32px" : "48px"}
+        display="flex"
+        flexDirection={isDesktop ? "row" : "column"}
+        gap="24px"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Typography
+          color="#ffffff"
+          fontSize={isDesktop ? "12px" : "14px"}
+          fontWeight={isDesktop ? "500" : "400"}
         >
-          Privacy Policy
-        </Button>
-        <Button
-          onClick={() => navigate("/terms-and-conditions")}
-          variant={"text"}
-          sx={{
-            textTransform: "none",
-            fontFamily: "InterMedium",
-            fontSize: "0.8rem",
-            textDecoration: "underline",
-            color: "#fff",
-            "&:hover": {
-              textDecoration: "underline",
-              backgroundColor: "transparent",
-              color: "#ababab",
-            },
-          }}
+          © 2021 Ecowave. All Rights Reserved.
+        </Typography>
+        <Box
+          component="div"
+          display="flex"
+          alignItems="center"
+          gap={isDesktop ? "24px" : "12px"}
         >
-          Terms and Conditions
-        </Button>
+          <Link
+            to={
+              import.meta.env.VITE_FACEBOOK_LINK || "https://www.facebook.com/"
+            }
+          >
+            {isDesktop ? (
+              <FaFacebook size={18} />
+            ) : (
+              <Box
+                borderRadius={"999px"}
+                bgcolor={"#01162C"}
+                width={"32px"}
+                height={"32px"}
+                display={"flex"}
+                alignItems={"center"}
+                justifyContent={"center"}
+              >
+                <FaFacebookF size={14} />
+              </Box>
+            )}
+          </Link>
+          <Link
+            to={
+              import.meta.env.VITE_INSTAGRAM_LINK ||
+              "https://www.instagram.com/"
+            }
+          >
+            {isDesktop ? (
+              <FaInstagram size={18} />
+            ) : (
+              <Box
+                borderRadius={"999px"}
+                bgcolor={"#01162C"}
+                width={"32px"}
+                height={"32px"}
+                display={"flex"}
+                alignItems={"center"}
+                justifyContent={"center"}
+              >
+                <FaInstagram size={14} />
+              </Box>
+            )}
+          </Link>
+          <Link
+            to={
+              import.meta.env.VITE_LINKEDIN_LINK || "https://www.linkedin.com/"
+            }
+          >
+            {isDesktop ? (
+              <FaLinkedinIn size={18} />
+            ) : (
+              <Box
+                borderRadius={"999px"}
+                bgcolor={"#01162C"}
+                width={"32px"}
+                height={"32px"}
+                display={"flex"}
+                alignItems={"center"}
+                justifyContent={"center"}
+              >
+                <FaLinkedinIn size={14} />
+              </Box>
+            )}
+          </Link>
+        </Box>
       </Box>
     </Box>
   );
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,65 +6,68 @@ import { Box, Divider } from "@mui/material";
 
 export const Footer = () => {
   return (
-    <Box
-      component="footer"
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "100%",
-        height: "auto",
-        backgroundColor: "#022241",
-        padding: "0 3.5rem",
-      }}
-    >
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "flex-start",
-          width: "100%",
-          height: "auto",
-          backgroundColor: "#022241",
-          padding: "4rem 0 4rem 0",
-        }}
-      >
-        <LogoFooter />
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            alignItems: "flex-start",
-            gap: "7rem",
-            paddingRight: "5rem",
-          }}
-        >
-          <NavigationFooter
-            title="Services"
-            links={[
-              { text: "Heat Pumps", url: "/heat-pumps" },
-              { text: "EV Chargers", url: "/ev-chargers" },
-              { text: "Solar Panels", url: "/solar-panels" },
-              { text: "Energy Storage", url: "/energy-storage" },
-            ]}
-          />
-          <NavigationFooter
-            title="Our Company"
-            links={[
-              { text: "About Us", url: "/about-us" },
-              { text: "Contact Us", url: "/contact-us" },
-            ]}
-          />
-          <FooterInfo />
-        </Box>
-      </Box>
-      <Divider
-        style={{ width: "100%", margin: "1rem 0", backgroundColor: "#D8D8DE" }}
-      />
+    <Box component="footer" sx={{ backgroundColor: "#022241" }}>
       <BottomFooter />
     </Box>
+    // <Box
+    //   component="footer"
+    //   sx={{
+    //     display: "flex",
+    //     flexDirection: "column",
+    //     justifyContent: "center",
+    //     alignItems: "center",
+    //     width: "100%",
+    //     height: "auto",
+    //     backgroundColor: "#022241",
+    //     padding: "0 3.5rem",
+    //   }}
+    // >
+    //   <Box
+    //     sx={{
+    //       display: "flex",
+    //       flexDirection: "row",
+    //       justifyContent: "space-between",
+    //       alignItems: "flex-start",
+    //       width: "100%",
+    //       height: "auto",
+    //       backgroundColor: "#022241",
+    //       padding: "4rem 0 4rem 0",
+    //     }}
+    //   >
+    //     <LogoFooter />
+    //     <Box
+    //       sx={{
+    //         display: "flex",
+    //         flexDirection: "row",
+    //         justifyContent: "space-between",
+    //         alignItems: "flex-start",
+    //         gap: "7rem",
+    //         paddingRight: "5rem",
+    //       }}
+    //     >
+    //       <NavigationFooter
+    //         title="Services"
+    //         links={[
+    //           { text: "Heat Pumps", url: "/heat-pumps" },
+    //           { text: "EV Chargers", url: "/ev-chargers" },
+    //           { text: "Solar Panels", url: "/solar-panels" },
+    //           { text: "Energy Storage", url: "/energy-storage" },
+    //         ]}
+    //       />
+    //       <NavigationFooter
+    //         title="Our Company"
+    //         links={[
+    //           { text: "About Us", url: "/about-us" },
+    //           { text: "Contact Us", url: "/contact-us" },
+    //         ]}
+    //       />
+    //       <FooterInfo />
+    //     </Box>
+    //   </Box>
+    //   <Divider
+    //     style={{ width: "100%", margin: "1rem 0", backgroundColor: "#D8D8DE" }}
+    //   />
+    //   <BottomFooter />
+    // </Box>
   );
 };
