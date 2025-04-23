180b49d682cfd2fd7ecd73aa4f0b7ec553578eed
sweetappleplus <sweetappleplus@gmail.com>
2025-04-23 07:58:21 +0300

    Improved the contact form in the homepage

--- src/components/forms/ContactComponent.tsx
+++ src/components/forms/ContactComponent.tsx
@@ -1,12 +1,27 @@
 import { LocationInfo } from "./LocationInfo";
 import { ContactForm } from "./ContactForm";
 import { Box, Theme, useMediaQuery } from "@mui/material";
+import waveVectorImage from "../../assets/images/wave-vector.png";
 
 export const ContactComponent = () => {
   const isDesktop = useMediaQuery((theme: Theme) => theme.breakpoints.up("sm"));
   return (
-    <Box component={"div"} bgcolor={"#F5F5F5"}>
+    <Box component={"div"} bgcolor={"#F5F5F5"} position={"relative"}>
+      <img
+        style={{
+          position: "absolute",
+          bottom: 0,
+          left: 0,
+          right: 0,
+          width: "100%",
+          height: "auto",
+        }}
+        src={waveVectorImage}
+        alt="wave-vector"
+      />
       <Box
+        zIndex={1}
+        position={"relative"}
         component={"div"}
         className="container"
         paddingTop={isDesktop ? "92px" : "44px"}
