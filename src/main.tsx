e29eecf127030660a982e853aad16969039314f4
sweetappleplus <sweetappleplus@gmail.com>
2025-04-25 11:34:17 +0300

    Added the inter font family

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { StrictMode } from "react";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./theme/themeConfig.ts";
 import "./fonts.css";
+import "./fonts-inter.css";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
