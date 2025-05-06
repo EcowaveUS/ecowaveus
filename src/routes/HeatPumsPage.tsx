64221717d95e569af490dbd31992f28a02a3d736
sweetappleplus <sweetappleplus@gmail.com>
2025-05-06 15:27:17 +0300

    Finished the heat pumps page

--- src/routes/HeatPumsPage.tsx
+++ src/routes/HeatPumsPage.tsx
@@ -1,55 +1,17 @@
 // import { useLocation } from "react-router-dom";
-import styled from "styled-components";
+// import styled from "styled-components";
 // import { motion } from "framer-motion";
 import { ServiceBenefits } from "../components/services/ServiceBenefits";
 import { OursBrands } from "../components/brands/OursBrands";
-import { ProccesCard } from "../components/services/ProccesCard";
-import { RebatesIncentives } from "../components/services/RebatesIncentives";
 import { brandImagesNew } from "../constants/data";
 import { Helmet } from "react-helmet";
 import { MainPageComponentHeatPump } from "../components/pagesComponents/MainPageComponentHeatPump";
 import { ContactComponent } from "../components/forms/ContactComponent";
 import { InstallationProcess } from "../components/pagesComponents/InstallationProcess";
-import { Box } from "@mui/material";
+import { Box, Theme, Typography, useMediaQuery } from "@mui/material";
 
 export const HeatPumsPage = () => {
-  // const location = useLocation();
-  // const pathLocation = location.pathname;
-
-  const stepByStepList = [
-    {
-      id: 1,
-      item: "01",
-      title: "Contact Us ",
-      content: "Contact us to discuss your project and schedule an appointment",
-    },
-    {
-      id: 2,
-      item: "02",
-      title: "Get a FREE Estimate",
-      content:
-        "Receive a free estimate and information on rebates and financing (including Mass Save 0% HEAT loan if applicable) - We will inform you and help you through the process",
-    },
-    {
-      id: 3,
-      item: "03",
-      title: "Contract and Schedule",
-      content: "Sign the contract and schedule the installation",
-    },
-    {
-      id: 4,
-      item: "04",
-      title: "Complete Installation",
-      content: "Proceed with the heat pump installation ",
-    },
-    {
-      id: 5,
-      item: "05",
-      title: "Rebates and Incentives Forms",
-      content: "We will help you to apply for rebates - ",
-      span: "Enjoy your new system!",
-    },
-  ];
+  const isDesktop = useMediaQuery((theme: Theme) => theme.breakpoints.up("sm"));
 
   return (
     <>
@@ -84,6 +46,64 @@ export const HeatPumsPage = () => {
         />
         <Box pt={"157px"} bgcolor={"#F6F8FA"}>
           <OursBrands color="#F6F8FA" images={brandImagesNew} />
+        </Box>
+        <Box
+          bgcolor={"#fff"}
+          borderRadius={"12px"}
+          component={"div"}
+          px={isDesktop ? "64px" : "32px"}
+          pt={isDesktop ? "64px" : "32px"}
+          pb={"24px"}
+          position={"absolute"}
+          bottom={isDesktop ? "180px" : "118px"}
+          width={"min(1280px, 100% - 40px)"}
+          left={"50%"}
+          sx={{
+            boxShadow: "0px 2px 12px 0px #7979850A",
+            transform: "translateX(-50%)",
+          }}
+        >
+          <Box maxWidth={"848px"}>
+            <Typography
+              fontFamily={"Inter !important"}
+              fontSize={isDesktop ? "48px" : "32px"}
+              fontWeight={600}
+              lineHeight={"100%"}
+              color={"#0B081F"}
+            >
+              Maximize your savings
+            </Typography>
+            <Typography
+              fontFamily={"Inter !important"}
+              fontSize={"18px"}
+              fontWeight={400}
+              lineHeight={"26px"}
+              color={"#0B081F"}
+              mt={"24px"}
+            >
+              Whole-home rebates are available to customers who install heat
+              pumps as the sole source of heating and cooling (up to $10,000).
+              <br />
+              <br />
+              Partial-home rebates are offered based on equipment or outdoor
+              unit (tonnage) to customers who plan to keep an existing boiler or
+              furnace in place to supplement a new heat pump system ($1,250 per
+              ton, up to $10,000).
+              <br />
+              <br />
+              <Typography
+                fontFamily={"Inter !important"}
+                fontSize={"20px"}
+                fontWeight={600}
+                lineHeight={"26px"}
+                color={"#0B081F"}
+              >
+                Federal Incentives
+              </Typography>
+              Federal tax credit 30% of the cost of the installation up to
+              $2,000
+            </Typography>
+          </Box>
         </Box>
       </Box>
       {/* <ImageBrackground>
@@ -153,126 +173,126 @@ export const HeatPumsPage = () => {
 //   min-height: 100vh;
 // `;
 
-const Text = styled.p`
-  color: #fff;
+// const Text = styled.p`
+//   color: #fff;
 
-  font-family: Roboto;
-  font-size: 1.5rem;
-  font-style: normal;
-  line-height: 1.8rem;
-  margin-bottom: 1rem;
-  text-align: center;
-`;
+//   font-family: Roboto;
+//   font-size: 1.5rem;
+//   font-style: normal;
+//   line-height: 1.8rem;
+//   margin-bottom: 1rem;
+//   text-align: center;
+// `;
 
-const ImageBrackground = styled.div`
-  width: 100%;
-  height: 100%;
-`;
-const StyledImage = styled.img`
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-`;
+// const ImageBrackground = styled.div`
+//   width: 100%;
+//   height: 100%;
+// `;
+// const StyledImage = styled.img`
+//   width: 100%;
+//   height: 100%;
+//   object-fit: cover;
+// `;
 
-const ProccesComponent = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: flex-start;
-  width: 75%;
-  border-radius: 16px;
-  background-color: #fff;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  padding: 3rem;
-  margin-top: -7rem;
-  margin-bottom: 8rem;
-  @media (max-width: 1024px) {
-    width: 90%;
-    align-items: center;
-    margin-top: -3rem;
-  }
-`;
-const ProccesCardWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: flex-start;
-  width: 60%;
-  @media (max-width: 1024px) {
-    width: 100%;
-    justify-content: center;
-  }
-`;
-const ProccesGridContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  width: 100%;
-  gap: 2rem;
-`;
-const ImageWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 50%;
-  @media (max-width: 1024px) {
-    display: none;
-  }
-`;
-const Image = styled.img`
-  width: 100%;
-  height: auto;
-  object-fit: cover;
-  border-radius: 16px;
-`;
-const SubTitle = styled.p`
-  color: #1c1f35;
+// const ProccesComponent = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   align-items: flex-start;
+//   width: 75%;
+//   border-radius: 16px;
+//   background-color: #fff;
+//   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+//   padding: 3rem;
+//   margin-top: -7rem;
+//   margin-bottom: 8rem;
+//   @media (max-width: 1024px) {
+//     width: 90%;
+//     align-items: center;
+//     margin-top: -3rem;
+//   }
+// `;
+// const ProccesCardWrapper = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   justify-content: flex-start;
+//   width: 60%;
+//   @media (max-width: 1024px) {
+//     width: 100%;
+//     justify-content: center;
+//   }
+// `;
+// const ProccesGridContainer = styled.div`
+//   display: flex;
+//   flex-direction: row;
+//   align-items: center;
+//   justify-content: space-between;
+//   width: 100%;
+//   gap: 2rem;
+// `;
+// const ImageWrapper = styled.div`
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   width: 50%;
+//   @media (max-width: 1024px) {
+//     display: none;
+//   }
+// `;
+// const Image = styled.img`
+//   width: 100%;
+//   height: auto;
+//   object-fit: cover;
+//   border-radius: 16px;
+// `;
+// const SubTitle = styled.p`
+//   color: #1c1f35;
 
-  font-family: Inter;
-  font-size: 14px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: normal;
-  border-left: 4px solid #68be54;
-  background: rgba(232, 232, 232, 0.5);
-  padding: 3px 9px 3px 8px;
-  margin-bottom: 0.5rem;
-  align-self: flex-start;
-`;
-const TitleProcces = styled.h3`
-  color: #1c1f35;
-  font-family: Inter;
-  font-size: 35px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: normal;
-  margin-bottom: 0.5rem;
-`;
-const TextProcces = styled.p`
-  color: #666c89;
-  font-family: Roboto;
-  font-size: 24px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 151.523%; /* 36.365px */
-`;
+//   font-family: Inter;
+//   font-size: 14px;
+//   font-style: normal;
+//   font-weight: 500;
+//   line-height: normal;
+//   border-left: 4px solid #68be54;
+//   background: rgba(232, 232, 232, 0.5);
+//   padding: 3px 9px 3px 8px;
+//   margin-bottom: 0.5rem;
+//   align-self: flex-start;
+// `;
+// const TitleProcces = styled.h3`
+//   color: #1c1f35;
+//   font-family: Inter;
+//   font-size: 35px;
+//   font-style: normal;
+//   font-weight: 700;
+//   line-height: normal;
+//   margin-bottom: 0.5rem;
+// `;
+// const TextProcces = styled.p`
+//   color: #666c89;
+//   font-family: Roboto;
+//   font-size: 24px;
+//   font-style: normal;
+//   font-weight: 400;
+//   line-height: 151.523%; /* 36.365px */
+// `;
 
-const BrandsWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  background-color: #fff;
-`;
+// const BrandsWrapper = styled.div`
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   width: 100%;
+//   background-color: #fff;
+// `;
 
-const IncentivesTitle = styled.h5`
-  color: var(--White, var(--fore-allways---white, #fff));
-  text-align: center;
-  font-family: Inter;
-  font-size: 32px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: 110%;
-  margin-top: 3rem;
-`;
+// const IncentivesTitle = styled.h5`
+//   color: var(--White, var(--fore-allways---white, #fff));
+//   text-align: center;
+//   font-family: Inter;
+//   font-size: 32px;
+//   font-style: normal;
+//   font-weight: 500;
+//   line-height: 110%;
+//   margin-top: 3rem;
+// `;
