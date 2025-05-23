146391345539693d5ef4e9ffdd4a6fd1b8c86aa7
sweetappleplus <sweetappleplus@gmail.com>
2025-05-23 11:13:53 +0530

    Made 50% of free estimate contact section

--- src/components/pagesComponents/freeEstimate/FreeEstimateContact.tsx
+++ src/components/pagesComponents/freeEstimate/FreeEstimateContact.tsx
@@ -1,3 +1,38 @@
+import { Box, Theme, useMediaQuery } from "@mui/material";
+import { ContactForm } from "../../forms/ContactForm";
+
 export default function FreeEstimateContact() {
-  return <div>Free Estimate Contact</div>;
+  const isDesktop = useMediaQuery((theme: Theme) => theme.breakpoints.up("md"));
+  return (
+    <Box
+      component={"div"}
+      sx={{
+        background: "url('/images/free-estimate-contact.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <Box
+        component={"div"}
+        className="container"
+        paddingTop={isDesktop ? "92px" : "48px"}
+        paddingBottom={isDesktop ? "92px" : "42px"}
+        display={"flex"}
+        flexDirection={isDesktop ? "row" : "column"}
+        gap={isDesktop ? "96px" : "24px"}
+        justifyContent={"space-between"}
+        alignItems={isDesktop ? "center" : "flex-start"}
+      >
+        <Box
+          component={"div"}
+          minWidth={isDesktop ? "600px" : "100%"}
+          id="contact-form"
+        >
+          <ContactForm btnColor="#022241" />
+        </Box>
+        <Box component={"div"} flex={1} display={"flex"}></Box>
+      </Box>
+    </Box>
+  );
 }
